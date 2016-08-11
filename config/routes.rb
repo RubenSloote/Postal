03870f5fac63c2272ad7c31ee4b1739e60b833ff
RubenSloote <rubensloote@gmail.com>
2016-08-11 13:44:08 +0200

    Like posts

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,11 @@ Rails.application.routes.draw do
   get 'posts/index'
 
   resources :posts
-  
+
   get 'user_posts/:user_id' => 'posts#user', as: :user_posts
+
+  resources :post do
+    resources :likes
+  end
 
 end
