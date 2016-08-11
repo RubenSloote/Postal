d51edb333ee04da076d6a09ea18886a930f07740
RubenSloote <rubensloote@gmail.com>
2016-08-11 11:36:43 +0200

    Form to create posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,14 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+  def create
+    post_params = params.require( :post ).permit( :content )
+    @post = Post.new( content: post_params[:content] )
+
+    if @post.save
+      redirect_to post_path
+    else
+      render post_path
+    end
+  end
 end
